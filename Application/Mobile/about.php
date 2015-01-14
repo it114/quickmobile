d30a16d8680779bc59ba0e3e1b487eec8b4b8c20
zxy <haoqqemail@qq.com>
2015-01-15 00:44:14 +0800

    0114

---
+++ Application/Mobile/about.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('IN_QM') or die('Access Denied.');
+return array(
+    'name' => '用户模块',
+    'version' => '1.0',
+    'desc' => '用户模块，用户的登录、注册、修改资料等逻辑',
+    'url' => 'http://www.quickmobile.cn',
+    'email' => 'xinyun78@gmail.com',
+    'author' => '邱君',
+    'author_url' => 'http://www.quickmobile.cn',
+);
